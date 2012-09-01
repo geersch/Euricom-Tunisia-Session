d7558c82acf7aac00705a9ac249b65a97ea79518
Christophe Geers <geersch@gmail.com>
2012-09-01 15:06:07 +0200

    Added App_Start files

---
+++ Euricom.Web.UI/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Euricom.Web.UI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
