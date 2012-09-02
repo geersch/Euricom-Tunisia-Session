a8a6ba389fde3d8018e0a2b99b2c7946b521da48
Christophe Geers <geersch@gmail.com>
2012-09-02 13:55:12 +0200

    Landing pages for the various topics + working top-nav bar

--- Euricom.Web.UI/Controllers/HomeController.cs
+++ Euricom.Web.UI/Controllers/HomeController.cs
@@ -8,5 +8,30 @@ namespace Euricom.Web.UI.Controllers
         {
             return View();
         }
+        
+        public ActionResult Github()
+        {
+            return View();
+        }
+
+        public ActionResult Bootstrap()
+        {
+            return View();
+        }
+
+        public ActionResult Knockout()
+        {
+            return View();
+        }
+
+        public ActionResult MongoDb()
+        {
+            return View();
+        }
+
+        public ActionResult AppHarbor()
+        {
+            return View();
+        }
     }
 }
